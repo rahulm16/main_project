f3360a8127e70a6ab213c6b7b3867765bc978800
rahulm16 <rahulmdhl1625@gmail,com>
2024-11-23 17:32:16 +0530

    now everything is working

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, render_template, request, jsonify, session, u
 from course_finder import find_relevant_courses  # Assuming the previous code is in course_finder.py
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
+from bson import ObjectId
 from bson.json_util import dumps
 from mistralai import Mistral
 from flask_bcrypt import Bcrypt
@@ -38,7 +39,7 @@ logging.getLogger("pymongo").setLevel(logging.WARNING)
 # Initialize Mistral client
 api_key = os.getenv("API_KEY")
 model = "mistral-large-latest"
-client = Mistral(api_key=api_key)
+mistral_client = Mistral(api_key=api_key)
 
 @app.route('/')
 def index():
@@ -330,7 +331,7 @@ def generate_questions(career_preferences):
         }
     ]
 
-    chat_response = client.chat.complete(
+    chat_response = mistral_client.chat.complete(
         model=model,
         messages=messages
     )
@@ -508,9 +509,9 @@ def fetch_suggestions():
     }
 
     user_responses = {
-        "Hi, I am SavvyAI! I'm here to help you choose a career. Can you share what you've always dreamed of becoming?": doc2["careerPreferences"]["first"],
-        "That's great, Is there something you're passionate about or find fascinating that you'd like to explore?": doc2["careerPreferences"]["second"],
-        "What other interests or skills would you like to develop into a career?": doc2["careerPreferences"]["third"]
+        "First priority": doc2["careerPreferences"]["first"],
+        "Second priority": doc2["careerPreferences"]["second"],
+        "Third priority": doc2["careerPreferences"]["third"]
     }
 
     if not questions:
@@ -539,9 +540,8 @@ def fetch_suggestions():
                f"\"upgrad_query\": \"Search query for UpGrad\", "
                f"\"nptel_keywords\": [\"keyword1\", \"keyword2\", \"keyword3\", \"keyword4\", \"keyword5\"]}}].")
                 
-    logging.debug(f"What are we sending to API:\n{content}\n\n\t\t----------------------------");
     try:
-        chat_response = client.chat.complete(
+        chat_response = mistral_client.chat.complete(
             model=model,
             messages=[{
                 "role": "user",
@@ -764,7 +764,7 @@ def fetch_detailed_layout():
 
             try:
                 # Make API request for each career
-                chat_response = client.chat.complete(
+                chat_response = mistral_client.chat.complete(
                     model=model,
                     messages=[{
                         "role": "user",
